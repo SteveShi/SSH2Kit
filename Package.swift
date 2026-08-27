8e7b63e27e53b84db40e455a3de15409addbe4fc
SteveShi <xuanlian@mac.com>
2026-08-28 01:02:52 +0800

    fix: rename package and targets to SSH2Kit in Package.swift and CI template

--- Package.swift
+++ Package.swift
@@ -2,29 +2,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "libssh2-swift",
+    name: "SSH2Kit",
     platforms: [
         .macOS(.v15)
     ],
     products: [
         .library(
-  name: "libssh2-swift",
-  targets: ["libssh2-swift"]
+            name: "SSH2Kit",
+            targets: ["SSH2Kit"]
         )
     ],
     targets: [
         .target(
-  name: "libssh2-swift",
-  dependencies: ["Clibssh2"]
+            name: "SSH2Kit",
+            dependencies: ["Clibssh2"]
         ),
         .target(
-  name: "Clibssh2",
-  dependencies: ["libssh2kit"]
+            name: "Clibssh2",
+            dependencies: ["libssh2kit"]
         ),
         .binaryTarget(
-  name: "libssh2kit",
-  url: "https://github.com/SteveShi/libssh2-swift/releases/download/v1.3.14/libssh2kit.xcframework.zip",
-  checksum: "6e8f961781f13bcf805254c2c943efa3629a817894916d176d5e7a91f06fd304"
+            name: "libssh2kit",
+            url: "https://github.com/SteveShi/SSH2Kit/releases/download/v1.3.14/libssh2kit.xcframework.zip",
+            checksum: "6e8f961781f13bcf805254c2c943efa3629a817894916d176d5e7a91f06fd304"
         )
     ]
 )
